caa04577b8ad2c918be03f758bb9c0424bed14e4
David Marom <davidmarom@hotmail.com>
2025-08-03 17:24:04 +0300

    wip

---
+++ src/manager.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from cleaner import clean_tweet_data
+
+
+df = pd.read_csv('tweets_dataset.csv', encoding='utf-8')
+clean_df = clean_tweet_data(df)
+
+print(clean_df.head())
+
+# clean_df.to_csv('../results/clean_tweets.csv', index=False)
