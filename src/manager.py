7b0fc141a4e08f3c7958ea04909527167fb9e45f
David Marom <davidmarom@hotmail.com>
2025-08-03 18:14:10 +0300

    fix csv

--- src/manager.py
+++ src/manager.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from cleaner import clean_tweet_data
 
-df = pd.read_csv('tweets_dataset.csv', encoding='utf-8', low_memory=False)
+df = pd.read_csv('../data/tweets_dataset.csv', encoding='utf-8', low_memory=False)
 clean_df = clean_tweet_data(df)
-
 
 clean_df.to_csv('../results/clean_tweets.csv', index=False)
